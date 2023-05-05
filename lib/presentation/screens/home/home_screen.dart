21d202f64593c1a228b19e4eb36f7479ac5f04a5
CTM <clemente1875gmail.com>
2023-05-05 20:08:24 +0200

    Home Screen creado

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class HomeScreeen extends StatelessWidget {
+  const HomeScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter + Material 3'),
+      ),
+    );
+  }
+}
