3fc463ab1883afe04aec92908824ce74588e15c6
CTM <clemente1875gmail.com>
2023-05-13 07:18:55 +0200

    Screens creadas y barril actualizado

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class AnimatedScreen extends StatelessWidget {
+
+  static const name = 'animatedScreen';
+  //constructor
+  const AnimatedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+class SnackbarScreen extends StatelessWidget {
+
+  static const name = 'snackbar_screen';
+  //constructor
+  const SnackbarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
